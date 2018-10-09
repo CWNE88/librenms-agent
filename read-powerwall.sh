e65023a588dce58fddc8f1d832d9117096324cc9
CWNE88 <38122076+CWNE88@users.noreply.github.com>
2018-10-09 20:41:10 +1000

    Add files via upload
    Script to read Powerwall variables that are gathered by powerwall-agent.py
    
    Place in /usr/local/bin/

---
+++ read-powerwall.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+/bin/cat /var/tmp/powerwall/powerwall_battery_charge
+
+/bin/cat /var/tmp/powerwall/powerwall_solar_power
+/bin/cat /var/tmp/powerwall/powerwall_solar_reactive_power
+/bin/cat /var/tmp/powerwall/powerwall_solar_apparent_power
+/bin/cat /var/tmp/powerwall/powerwall_solar_voltage
+/bin/cat /var/tmp/powerwall/powerwall_solar_current
+/bin/cat /var/tmp/powerwall/powerwall_solar_frequency
+/bin/cat /var/tmp/powerwall/powerwall_solar_exported_today
+/bin/cat /var/tmp/powerwall/powerwall_solar_imported_today
+
+/bin/cat /var/tmp/powerwall/powerwall_load_power
+/bin/cat /var/tmp/powerwall/powerwall_load_reactive_power
+/bin/cat /var/tmp/powerwall/powerwall_load_apparent_power
+/bin/cat /var/tmp/powerwall/powerwall_load_voltage
+/bin/cat /var/tmp/powerwall/powerwall_load_current
+/bin/cat /var/tmp/powerwall/powerwall_load_frequency
+/bin/cat /var/tmp/powerwall/powerwall_load_exported_today
+/bin/cat /var/tmp/powerwall/powerwall_load_imported_today
+
+/bin/cat /var/tmp/powerwall/powerwall_battery_power
+/bin/cat /var/tmp/powerwall/powerwall_battery_reactive_power
+/bin/cat /var/tmp/powerwall/powerwall_battery_apparent_power
+/bin/cat /var/tmp/powerwall/powerwall_battery_voltage
+/bin/cat /var/tmp/powerwall/powerwall_battery_current
+/bin/cat /var/tmp/powerwall/powerwall_battery_frequency
+/bin/cat /var/tmp/powerwall/powerwall_battery_exported_today
+/bin/cat /var/tmp/powerwall/powerwall_battery_imported_today
+
+/bin/cat /var/tmp/powerwall/powerwall_grid_power
+/bin/cat /var/tmp/powerwall/powerwall_grid_reactive_power
+/bin/cat /var/tmp/powerwall/powerwall_grid_apparent_power
+/bin/cat /var/tmp/powerwall/powerwall_grid_voltage
+/bin/cat /var/tmp/powerwall/powerwall_grid_current
+/bin/cat /var/tmp/powerwall/powerwall_grid_frequency
+/bin/cat /var/tmp/powerwall/powerwall_grid_exported_today
+/bin/cat /var/tmp/powerwall/powerwall_grid_imported_today
+
